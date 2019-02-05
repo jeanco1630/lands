afb2afb5d12f362aead1344956e12f1aec9ab7f6
fernandez1630@gmail.com <Jeanco163025>
2019-02-05 11:26:16 -0400

    LOGIN

---
+++ Lands/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿
+
+
+namespace Lands
+{
+    using Views;
+    using Xamarin.Forms;
+    public partial class App : Application
+    {
+        #region Construtors
+        public App()
+        {
+            InitializeComponent();
+
+            this.MainPage = new NavigationPage (new LoginPage());
+        }
+        #endregion
+
+        #region Methods 
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+        #endregion
+    }
+}
